5f76bbd9356798d66aedd3d611b3ca740b8bb6a5
Tina Ryu <hyunsury@usc.edu>
2019-05-26 19:02:03 -0700

    Adds materials

---
+++ Unity/Assets/Mars Model Pack v1/Scripts/DoorStartOpen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorStartOpen : MonoBehaviour {
+
+    [HideInInspector]
+    public bool isDoorOpen = false;
+
+    // Use this for initialization
+	void Start ()
+    {
+		if(GetComponent<Animator>() != null)
+        {
+            GetComponent<Animator>().SetBool("shouldOpen", true);
+            isDoorOpen = true;
+        }
+	}
+	
+	
+}
