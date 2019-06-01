bb074f8a6a4860f4faa6fa6b88254e8c0496718d
cfletche <cfletche@usc.edu>
2019-06-01 12:38:54 -0700

    Delete

--- Unity/Assets/Heavy Station Kit/Common/HideCursor.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class HideCursor : MonoBehaviour {
-	void Start () {		
-		Cursor.visible = false;
-		Cursor.lockState = CursorLockMode.Locked;
-	}
-}
--- Unity/Assets/Heavy Station Kit/Common/Vent/DotHskVentFixColony.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class DotHskVentFixColony : StateMachineBehaviour {
-	public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerInfo){
-		animator.transform.localPosition = new Vector3(-4.42f,1f,0f);
-	}
-}
